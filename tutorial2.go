3e200787b24fbe372ee7942620496df2d2fb8baf
Alok <alok5633y@gmail.com>
2022-02-04 17:18:56 +0530

    tutorial 2 added comments

--- tutorial2.go
+++ tutorial2.go
@@ -9,10 +9,12 @@ import (
 
 
 func main() {
+	//Reading input
 	Scanner := bufio.NewScanner(os.Stdin)
 	Scanner.Scan()
 	inp := Scanner.Text()
 	fmt.Println(inp)
+	//Conversion from string to int
 	no, _ := strconv.ParseInt(inp, 10, 64)
 	fmt.Println(1000 - no)
 }
